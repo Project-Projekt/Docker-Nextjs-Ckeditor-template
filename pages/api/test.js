5a691e7cad1f47d37b5eff779bd778d1cdf13e74
WheatleyTheCore <alex.colwell@icloud.com>
2023-06-26 09:55:45 -0400

    added todo

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+
+//TODO handle upload and return url as per https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/simple-upload-adapter.html
+
 export default function test(req, res) {
     console.log('it works!')
   }
