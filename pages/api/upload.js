f6d4b3bc7dec86f57a267ed2b5398a9aca4596ae
WheatleyTheCore <alex.colwell@icloud.com>
2023-06-29 10:04:06 -0400

    replaced image upload guid with image hash and we now just return the first version of the upload if someone tries to upload duplicates

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,6 +2,7 @@ import { randomUUID } from 'crypto'
 import { writeFile } from 'fs/promises'
 import formidable from "formidable";
 const fs = require('fs')
+const crypto = require('crypto')
 
 export const config = {
   api: {
@@ -15,19 +16,23 @@ export default async function upload(request, response) {
 
   if (!files) {
     console.log('no file!')
-    return NextResponse.json({ error: {message: "There was an error uploading the file"} })
+    return NextResponse.json({ error: { message: "There was an error uploading the file" } })
   }
 
   const filePath = files.upload[0].filepath
   let fileName = files.upload[0].originalFilename
   const data = fs.readFileSync(filePath);
 
-  // With the file data in the buffer, you can do whatever you want with it.
-  // For this, we'll just write it to the filesystem in a new location
 
-  fileName = randomUUID() + fileName
-  const path = `./public/uploads/${fileName}`
-  await writeFile(path, data)
+  const digest = crypto.createHash("sha256").update('secretkey').digest('base64') // hash the image so we can match based on image content
+  fileName = digest + '_' + fileName
+  let allUploads = fs.readdirSync('./public/uploads')
 
-  response.status(200).json({ url: `http://localhost:3000/uploads/${fileName}` })
+  if (allUploads.includes(fileName)) {
+    response.status(200).json({ url: `http://localhost:3000/uploads/${fileName}` })
+  } else {
+    const path = `./public/uploads/${fileName}`
+    await writeFile(path, data)
+    response.status(200).json({ url: `http://localhost:3000/uploads/${fileName}` })
+  }
 }
